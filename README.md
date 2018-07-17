eccf706280ace7e0dc70fa7532a1dabfd24764ac
Ryoichi Obara <ryoichi-obara@users.noreply.github.com>
2018-07-17 20:29:42 +0900

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# cloudwatch-logs-downloader
+# CloudWatchLogs Downloader
+
 Save CloudWatchLogs to local file.
+
+## Environment variables
+
+* ``PROFILE``: AWS Account profile name (To use default, comment-out 14-15th line.)
+* ``LOG_GROUP_NAME``: CloudWatch Log Groups Name, e.g. ``/aws/lambda/my-lambda-function``
+* ``REGION``
+* ``TIMEZONE``: For moment-timezone. e.g. ``Asia/Tokyo``
+* ``OUTPUT_LOG_FILENAME``: Filename to save logs. e.g. ``my-lambda-function.log``
+
+## Run
+
+```
+npm run main
+```
---
+++ index.js
@@ -0,0 +1,34 @@
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+const moment = require('moment-timezone');
+
+require('dotenv').config();
+
+const profile = process.env.PROFILE;
+const logGroupName = process.env.LOG_GROUP_NAME;
+const region = process.env.REGION;
+const tz = process.env.TIMEZONE;
+const filename = process.env.OUTPUT_LOG_FILENAME;
+
+// For devlocal
+const credentials = new AWS.SharedIniFileCredentials({ profile });
+AWS.config.credentials = credentials;
+
+const cloudwatchlogs = new AWS.CloudWatchLogs({ region });
+
+cloudwatchlogs.describeLogStreams({ logGroupName }, (err, res) => {
+  // console.log(res);
+
+  for (const logStream of res.logStreams) {
+    console.log(logStream.logStreamName);
+
+    cloudwatchlogs.getLogEvents({ logGroupName, logStreamName: logStream.logStreamName }, (error, events) => {
+      for (const event of events.events) {
+        const datetime = moment(event.timestamp).tz(tz).format('YYYY-MM-DD HH:mm:ss');
+        // console.log(`${datetime} ${event.message}`);
+        fs.appendFileSync(path.join(__dirname, filename), `${datetime} ${event.message}`);
+      }
+    });
+  }
+});
